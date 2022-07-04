c0557737637205a5875084326b376da04450578d
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 00:25:59 +0300

    Last try

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -7,7 +7,8 @@ public class TriangleSidesValidator {
                     && secondSide + thirdSide <= firstSide || firstSide == secondSide
                     || thirdSide == secondSide || firstSide == thirdSide) {
                 System.out.println("this is a valid triangle");
-
+            } else {
+                System.out.println("it's not a triangle");
             }
         } else {
             System.out.println("it's not a triangle");
